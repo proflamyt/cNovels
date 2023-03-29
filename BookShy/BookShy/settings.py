0f3e4a781dab56e839303623c14ef9fcbb893443
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-03-29 23:34:46 +0100

    changed title

--- BookShy/BookShy/settings.py
+++ BookShy/BookShy/settings.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(path.join(BASE_DIR, "../.env"))
+load_dotenv("/etc/secrets/.env")
 
 
 # Quick-start development settings - unsuitable for production
